2f0af5933c24d049be2ca3df9f6041dc5cc453c6
Julliet Nyaware <julienyaware@gmail.com>
2022-07-26 15:24:52 +0300

    Created login and session functionality using nextauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,8 @@ import NextAuth from 'next-auth'
 import EmailProvider from 'next-auth/providers/email'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import prisma from 'lib/prisma'
+//import prisma from './../../lib/prisma'
+
 
 export default NextAuth({
   providers: [
