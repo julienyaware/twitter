f44dd7b82db098a9aa6ab331d727fb625503cb6a
Julliet Nyaware <julienyaware@gmail.com>
2022-08-03 11:56:17 +0300

    Added feature of adding a tweet to the DB

--- pages/home.js
+++ pages/home.js
@@ -1,10 +1,11 @@
 import { useSession } from "next-auth/react";
+import NewTweet from "components/NewTweet";
 
 export default function Home () {
     const { data: session, status } = useSession()
   return (
     <div>
-      {session ? <p>You are logged in!</p> : <p>You are not logged in 😞</p>}
+      {session ? <NewTweet/> : <p>You are not logged in 😞</p>}
     </div>
   )
 }
